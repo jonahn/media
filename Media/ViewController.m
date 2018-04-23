e04984df78ffca92d2df75b5b36b78ede92c9768
ningbo <wangningbo@mafengwo.com>
2018-04-23 14:34:50 +0800

    音频 视频采集

---
+++ Media/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Media
+//
+//  Created by Jonathan on 2018/4/23.
+//  Copyright © 2018年 WWeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
